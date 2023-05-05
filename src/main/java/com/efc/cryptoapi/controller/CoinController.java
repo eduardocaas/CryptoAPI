9ccff0fcc55fbb92e0abbf728754d1f24cf98de2
eduardocaas <eduardofc.dev@gmail.com>
2023-05-04 23:15:23 -0300

    Controller for Coin with Insert and FindAll methods

---
+++ src/main/java/com/efc/cryptoapi/controller/CoinController.java
@@ -0,0 +1,36 @@
+package com.efc.cryptoapi.controller;
+
+import com.efc.cryptoapi.entity.Coin;
+import com.efc.cryptoapi.repository.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+
+@RestController
+@RequestMapping("/coin")
+public class CoinController {
+
+  @Autowired
+  private CoinRepository coinRepository;
+
+  @GetMapping
+  public ResponseEntity findAll(){
+    return new ResponseEntity<>(coinRepository.findAll(), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity insert(@RequestBody Coin coin){
+    try {
+      coin.setDateTime(new Timestamp(System.currentTimeMillis()));
+      return new ResponseEntity<>(coinRepository.save(coin), HttpStatus.CREATED);
+    } catch (Exception error){
+      return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+
+
+}
